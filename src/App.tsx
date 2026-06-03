20a0ea20755b72f832b2a01824bc60a92ac3b7c8
monesir <monesir@github.com>
2026-06-03 15:17:17 +0300

    fix: Use HashRouter instead of BrowserRouter for Electron file:// support

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import LiveTV from './pages/LiveTV';
 import WorldCup from './pages/WorldCup';
@@ -14,7 +14,7 @@ const App: React.FC = () => {
 
   return (
     <WorldCupProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<LiveTV />} />
@@ -24,7 +24,7 @@ const App: React.FC = () => {
             <Route path="settings" element={<Settings />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </WorldCupProvider>
   );
 };
